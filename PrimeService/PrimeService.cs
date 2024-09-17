f566d9c178eece5bacd52dcb73e869ac4138edf6
Dan Joe <sokits94@gmail.com>
2024-09-17 09:39:12 -0500

    First Commit

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        if (candidate == 1)
+        {
+        return false;
+        }
+        throw new NotImplementedException("Not fully implemented.");
+    }
+}
